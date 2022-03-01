32915702bf812c4dfbf73e7cd4f13b29aa3de0f9
Alex Ramirez <alexander.ramirez@gmail.com>
2022-03-01 14:16:37 -0500

    fix: Fix production NODE_ENV for Heroku

--- libs/api/config/feature/src/lib/api-config-feature.controller.ts
+++ libs/api/config/feature/src/lib/api-config-feature.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common'
 import { ApiConfigDataAccessService } from '@mogami/api/config/data-access'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('config')
 @Controller('config')
 export class ApiConfigFeatureController {
   constructor(private readonly service: ApiConfigDataAccessService) {}
