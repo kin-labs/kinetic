7d6a83e0ef334be5f0941eecf9048c4b1ba731e4
Bram Borggreve <borggreve@gmail.com>
2022-03-08 16:50:55 -0500

    fix: make MOGAMI_MINT_PUBLIC_KEY env var required

--- libs/api/config/feature/src/lib/config/validation-schema.ts
+++ libs/api/config/feature/src/lib/config/validation-schema.ts
@@ -4,7 +4,7 @@ export const validationSchema = Joi.object({
   DATABASE_URL: Joi.string().required(),
   MINIMUM_KIN_VERSION: Joi.number().default(5),
   MOGAMI_SUBSIDIZER_SECRET_KEY: Joi.string(),
-  MOGAMI_MINT_PUBLIC_KEY: Joi.string(),
+  MOGAMI_MINT_PUBLIC_KEY: Joi.string().required(),
   NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
   PORT: Joi.number().default(3000),
   SOLANA_RPC_ENDPOINT: Joi.string()
