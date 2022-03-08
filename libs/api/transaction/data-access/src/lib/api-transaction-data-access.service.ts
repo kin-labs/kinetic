f822e3734b2eae94cd57d2ee6d668b6a1a94932c
Bram Borggreve <borggreve@gmail.com>
2022-03-08 16:13:18 -0500

    fix: use the single Solana instance instantiated in ApiCoreDataAccessService

--- libs/api/transaction/data-access/src/lib/api-transaction-data-access.service.ts
+++ libs/api/transaction/data-access/src/lib/api-transaction-data-access.service.ts
@@ -1,5 +1,4 @@
 import { ApiCoreDataAccessService } from '@mogami/api/core/data-access'
-import { Solana } from '@mogami/solana'
 import { Injectable } from '@nestjs/common'
 import { HistoryResponse } from './entities/history.entity'
 import { MinimumKinVersionResponse } from './entities/minimum-kin-version.entity'
@@ -10,11 +9,7 @@ import { SubmitTransactionResponse } from './entities/submit-transaction.entity'
 
 @Injectable()
 export class ApiTransactionDataAccessService {
-  readonly solana: Solana
-
-  constructor(readonly data: ApiCoreDataAccessService) {
-    this.solana = new Solana(this.data.config.solanaRpcEndpoint)
-  }
+  constructor(readonly data: ApiCoreDataAccessService) {}
 
   getServiceConfig(): ServiceConfigResponse {
     return this.data.config.getServiceConfig()
@@ -25,11 +20,11 @@ export class ApiTransactionDataAccessService {
   }
 
   getRecentBlockhash(): Promise<RecentBlockhashResponse> {
-    return this.solana.getRecentBlockhash()
+    return this.data.solana.getRecentBlockhash()
   }
 
   getMinimumBalanceForRentExemption(dataLength: number): Promise<number> {
-    return this.solana.getMinimumBalanceForRentExemption(dataLength)
+    return this.data.solana.getMinimumBalanceForRentExemption(dataLength)
   }
 
   getHistory(): HistoryResponse {
