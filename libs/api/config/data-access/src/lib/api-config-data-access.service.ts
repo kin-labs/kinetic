d9496396bcff6a83daa0315e8fdf8f4b5330f84a
Bram Borggreve <borggreve@gmail.com>
2022-03-08 16:12:16 -0500

    fix: work around breaking import from TOKEN_PROGRAM_ID

--- libs/api/config/data-access/src/lib/api-config-data-access.service.ts
+++ libs/api/config/data-access/src/lib/api-config-data-access.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { PublicKey } from '@solana/web3.js'
+
+// TODO Figure out why this import breaks the build
+// Related issue: https://github.com/kin-labs/mogami/issues/26
+// import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
+
+const TOKEN_PROGRAM_ID = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
 
 @Injectable()
 export class ApiConfigDataAccessService {
