1c18d9f73c783e21870724af811873368be9d0e3
Bram Borggreve <borggreve@gmail.com>
2022-03-17 17:13:48 -0500

    test: make demo-e2e test run

--- apps/demo-e2e/src/integration/app.spec.ts
+++ apps/demo-e2e/src/integration/app.spec.ts
@@ -1,4 +1,4 @@
-import { getGreeting } from '../support/app.po'
+import { getHeader } from '../support/app.po'
 
 describe('demo', () => {
   beforeEach(() => cy.visit('/'))
@@ -8,6 +8,6 @@ describe('demo', () => {
     cy.login('my-email@something.com', 'myPassword')
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome demo')
+    getHeader().contains('Mogami')
   })
 })
