ceeb29c392c37473aa172ac884b33af16a512ee9
beeman <borggreve@gmail.com>
2022-03-29 15:02:00 -0500

    chore: disable running Nx Agents on CI (#77)
    * chore: disable running Nx Agents on CI
    
    * test: fix breaking Airdrop unit tests

--- libs/api/airdrop/data-access/src/lib/api-airdrop-data-access.service.spec.ts
+++ libs/api/airdrop/data-access/src/lib/api-airdrop-data-access.service.spec.ts
@@ -1,3 +1,4 @@
+import { ApiCoreDataAccessModule } from '@mogami/api/core/data-access'
 import { Test } from '@nestjs/testing'
 import { ApiAirdropDataAccessService } from './api-airdrop-data-access.service'
 
@@ -6,6 +7,7 @@ describe('ApiAirdropDataAccessService', () => {
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
+      imports: [ApiCoreDataAccessModule],
       providers: [ApiAirdropDataAccessService],
     }).compile()
 
