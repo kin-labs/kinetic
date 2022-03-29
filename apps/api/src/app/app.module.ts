5baaf3280f0de78481cbcd67b7c4f90ddd4817d4
Bram Borggreve <borggreve@gmail.com>
2022-03-29 15:09:17 -0500

    feat: make api host the admin

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,26 @@
-import { Module } from '@nestjs/common'
+import { Logger, Module } from '@nestjs/common'
 import { ApiCoreFeatureModule } from '@mogami/api/core/feature'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { ensureDirSync, existsSync, writeFileSync } from 'fs-extra'
+import { join } from 'path'
+
+const rootPath = join(__dirname, '..', 'admin')
 
 @Module({
-  imports: [ApiCoreFeatureModule],
+  imports: [
+    ApiCoreFeatureModule,
+    ServeStaticModule.forRoot({
+      rootPath,
+      exclude: ['/api/*', '/graphql'],
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    if (!existsSync(rootPath)) {
+      ensureDirSync(rootPath)
+      writeFileSync(join(rootPath, 'index.html'), `<pre>Mogami Api</pre>`)
+      Logger.verbose(`Created static root path ${rootPath}`)
+    }
+  }
+}
