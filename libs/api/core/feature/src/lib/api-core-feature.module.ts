9aa9610c71ebf42f9ae200fa8f28f84ee49dc50a
beeman <borggreve@gmail.com>
2022-04-01 00:59:48 -0500

    refactor: move @nestjs/serve-static from AppModule to ApiCoreFeatureModule (#83)

--- libs/api/core/feature/src/lib/api-core-feature.module.ts
+++ libs/api/core/feature/src/lib/api-core-feature.module.ts
@@ -6,9 +6,11 @@ import { ApiTransactionFeatureModule } from '@mogami/api/transaction/feature'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import { join } from 'path'
 import { ApiCoreFeatureController } from './api-core-feature.controller'
 import { ApiCoreFeatureResolver } from './api-core-feature.resolver'
+import { serveStaticFactory } from './serve-static.factory'
 
 @Module({
   controllers: [ApiCoreFeatureController],
@@ -19,6 +21,7 @@ import { ApiCoreFeatureResolver } from './api-core-feature.resolver'
       context: ({ req, res }) => ({ req, res }),
       driver: ApolloDriver,
     }),
+    ServeStaticModule.forRootAsync({ useFactory: serveStaticFactory() }),
     ApiAccountFeatureModule,
     ApiAirdropFeatureModule,
     ApiConfigFeatureModule,
