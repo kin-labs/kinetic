2ddd3e80b948e6b118e5d012f74d0be3a98ef8ae
Bram Borggreve <borggreve@gmail.com>
2022-03-31 20:22:34 -0500

    refactor: move @nestjs/serve-static from AppModule to ApiCoreFeatureModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,26 +1,7 @@
-import { Logger, Module } from '@nestjs/common'
 import { ApiCoreFeatureModule } from '@mogami/api/core/feature'
-import { ServeStaticModule } from '@nestjs/serve-static'
-import { ensureDirSync, existsSync, writeFileSync } from 'fs-extra'
-import { join } from 'path'
-
-const rootPath = join(__dirname, '..', 'admin')
+import { Module } from '@nestjs/common'
 
 @Module({
-  imports: [
-    ApiCoreFeatureModule,
-    ServeStaticModule.forRoot({
-      rootPath,
-      exclude: ['/api/*', '/graphql'],
-    }),
-  ],
+  imports: [ApiCoreFeatureModule],
 })
-export class AppModule {
-  constructor() {
-    if (!existsSync(rootPath)) {
-      ensureDirSync(rootPath)
-      writeFileSync(join(rootPath, 'index.html'), `<pre>Mogami Api</pre>`)
-      Logger.verbose(`Created static root path ${rootPath}`)
-    }
-  }
-}
+export class AppModule {}
