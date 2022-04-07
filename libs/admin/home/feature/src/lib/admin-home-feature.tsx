0cae329fc5cdb3e52e471d33a7983bb500f0d076
Bram Borggreve <borggreve@gmail.com>
2022-04-06 19:08:13 -0500

    feat: implement lazy loading of feature modules

--- libs/admin/home/feature/src/lib/admin-home-feature.tsx
+++ libs/admin/home/feature/src/lib/admin-home-feature.tsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react'
 import { useUptimeQuery } from '@mogami/shared/util/admin-sdk'
 import React from 'react'
 
-export function AdminHomeFeature() {
+export default function AdminHomeFeature() {
   const [{ data }] = useUptimeQuery()
   return (
     <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
