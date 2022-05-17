bb827105f650feff49716639e2a7f1920ab110ab
Bram Borggreve <borggreve@gmail.com>
2022-05-17 17:45:55 +0100

    fix: make unit test for api-cluster-data-access work

--- libs/api/cluster/data-access/src/lib/api-cluster-data-access.service.spec.ts
+++ libs/api/cluster/data-access/src/lib/api-cluster-data-access.service.spec.ts
@@ -1,3 +1,4 @@
+import { ApiCoreDataAccessModule } from '@mogami/api/core/data-access'
 import { Test } from '@nestjs/testing'
 import { ApiClusterDataAccessService } from './api-cluster-data-access.service'
 
@@ -6,6 +7,7 @@ describe('ApiClusterDataAccessService', () => {
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
+      imports: [ApiCoreDataAccessModule],
       providers: [ApiClusterDataAccessService],
     }).compile()
 
