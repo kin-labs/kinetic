64a8b5d5d18d715adee6979677a35556d4fa89ba
Bram Borggreve <borggreve@gmail.com>
2022-05-20 16:36:23 +0100

    feat: add sdk-e2e app

---
+++ apps/sdk-e2e/src/integration/mogami-sdk.spec.ts
@@ -0,0 +1,14 @@
+// import { MogamiSdk } from '@mogami/sdk'
+
+describe('Mogami Sdk (e2e)', () => {
+  // let sdk: MogamiSdk
+
+  beforeEach(async () => {
+    // sdk = await MogamiSdk.setup({ index: 1, endpoint: 'devnet ' })
+  })
+
+  it('should initialize SDK', () => {
+    // expect(sdk.config().app.index).toEqual(1)
+    expect(true).toBeTruthy()
+  })
+})
