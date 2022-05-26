f3cfbe5ac3ac94d2a8d1eb15f07aae887c4a4733
Bram Borggreve <borggreve@gmail.com>
2022-05-26 02:25:52 +0100

    ci: run sdk-e2e separate from others

--- apps/sdk-e2e/src/integration/mogami-sdk.spec.ts
+++ apps/sdk-e2e/src/integration/mogami-sdk.spec.ts
@@ -1,14 +1,17 @@
-// import { MogamiSdk } from '@mogami/sdk'
+import { MogamiSdk } from '@mogami/sdk'
+import { logger } from 'nx/src/utils/logger'
 
 describe('Mogami Sdk (e2e)', () => {
-  // let sdk: MogamiSdk
+  let sdk: MogamiSdk
 
   beforeEach(async () => {
-    // sdk = await MogamiSdk.setup({ index: 1, endpoint: 'devnet ' })
+    sdk = await MogamiSdk.setup({ index: 1, endpoint: 'devnet' })
+    logger.info('beforeEach')
   })
 
   it('should initialize SDK', () => {
-    // expect(sdk.config().app.index).toEqual(1)
+    expect(sdk.config().app.index).toEqual(1)
+    expect(sdk.config().app.name).toEqual('App 1')
     expect(true).toBeTruthy()
   })
 })
