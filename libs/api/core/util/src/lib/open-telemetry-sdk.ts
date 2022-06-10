fa2d41b7195accbb7780274ce1c31740d72a468e
Bram Borggreve <borggreve@gmail.com>
2022-06-10 01:33:56 +0100

    feat: expose Prometheus metrics through a Nest controller

--- libs/api/core/util/src/lib/open-telemetry-sdk.ts
+++ libs/api/core/util/src/lib/open-telemetry-sdk.ts
@@ -1,9 +1,11 @@
 import { Logger } from '@nestjs/common'
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus'
 import { NodeSDK } from '@opentelemetry/sdk-node'
+import { ServerResponse } from 'http'
 
 export class OpenTelementrySdk {
   static sdk: NodeSDK
+  private static metricExporter: PrometheusExporter
 
   static start(metricsEnabled: boolean) {
     if (!metricsEnabled) {
@@ -12,13 +14,13 @@ export class OpenTelementrySdk {
     }
     Logger.verbose(`Metrics are enabled`, 'OpenTelementrySdk')
 
-    const metricExporter = new PrometheusExporter({
+    this.metricExporter = new PrometheusExporter({
       prefix: 'mogami',
-      preventServerStart: !metricsEnabled,
+      preventServerStart: true,
     })
 
     this.sdk = new NodeSDK({
-      metricExporter,
+      metricExporter: this.metricExporter,
       metricInterval: 1000,
     })
 
@@ -27,5 +29,13 @@ export class OpenTelementrySdk {
 
   static async shutdown() {
     await this.sdk.shutdown()
+  }
+
+  static getMetrics(response: ServerResponse) {
+    if (!this.metricExporter) {
+      response.setHeader('content-type', 'text/plain')
+      return response.end('Metrics are disabled')
+    }
+    return this.metricExporter.getMetricsRequestHandler(undefined, response)
   }
 }
