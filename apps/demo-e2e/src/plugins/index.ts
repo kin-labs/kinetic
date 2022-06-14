ce804a06ff5db99aae3bbcd050de9967d46399cb
Alex Ramirez <alexander.ramirez@gmail.com>
2022-06-14 08:25:35 -0500

    feat: Implement Cypress videos deletion

--- apps/demo-e2e/src/plugins/index.ts
+++ apps/demo-e2e/src/plugins/index.ts
@@ -1,8 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const del = require('del')
+
 module.exports = (on, config) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('@cypress/code-coverage/task')(on, config)
 
   // add other tasks to be registered here
+  on('after:spec', (spec, results) => {
+    if (results && results.stats.failures === 0 && results.video) {
+      return del(results.video, {
+        force: true,
+      })
+    }
+  })
 
   // IMPORTANT to return the config object
   // with the any changed environment variables
