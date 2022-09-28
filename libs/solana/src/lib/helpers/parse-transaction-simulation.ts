1fcdf859b444f60b51d554484e18fa008b9d3db4
Bram Borggreve <beeman@beeman.nl>
2022-09-28 22:08:17 +0100

    chore: update Nx to latest

--- libs/solana/src/lib/helpers/parse-transaction-simulation.ts
+++ libs/solana/src/lib/helpers/parse-transaction-simulation.ts
@@ -29,8 +29,9 @@ function errorMessageMap(index: number | string | undefined) {
 export function parseTransactionSimulation(simulation: SimulatedTransactionResponse) {
   const logs = simulation.logs ? simulation.logs : undefined
   if (simulation?.err && typeof simulation.err === 'object' && 'InstructionError' in simulation.err) {
-    const [instructionIndex, instructionError] = simulation.err['InstructionError'] as [number, string?]
-    const { Custom } = instructionError as any
+    const simulationError = simulation.err as unknown as { InstructionError: [number, string?] }
+    const [instructionIndex, instructionError] = simulationError['InstructionError']
+    const { Custom } = instructionError as unknown as { Custom: number }
     const error = Custom !== undefined ? errorMessageMap(Custom) : errorMessageMap(instructionError)
     throw new TransactionError(error.message, logs, error.type, instructionIndex)
   }
