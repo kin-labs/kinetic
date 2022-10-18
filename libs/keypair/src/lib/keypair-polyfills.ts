91d789cc9d33e6e03fb5c433abf8e82821dc1456
Bram Borggreve <beeman@beeman.nl>
2022-10-18 12:48:05 +0100

    fix: add Buffer polyfill to Keypair class

--- libs/keypair/src/lib/keypair-polyfills.ts
+++ libs/keypair/src/lib/keypair-polyfills.ts
@@ -1,3 +1,10 @@
+// Polyfill the browser, prevents the error "Uncaught ReferenceError: Buffer is not defined"
+if (typeof window !== 'undefined' && typeof window.global === 'undefined' && typeof Buffer === 'undefined') {
+  window['global'] = window
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  global.Buffer = require('buffer').Buffer
+}
+
 if (typeof TextEncoder === 'undefined' || typeof window === 'undefined') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   global.TextEncoder = require('util').TextEncoder
