475c27ee4b7a548e8f3696b99be365342f961211
Bram Borggreve <beeman@beeman.nl>
2022-11-02 16:46:00 +0000

    chore: tag version 1.0.0-rc.7

--- libs/api/core/data-access/src/version.ts
+++ libs/api/core/data-access/src/version.ts
@@ -1,2 +1,2 @@
 export const NAME = '@kin-kinetic/api'
-export const VERSION = '1.0.0-rc.6'
+export const VERSION = '1.0.0-rc.7'
--- libs/sdk/src/version.ts
+++ libs/sdk/src/version.ts
@@ -1,2 +1,2 @@
 export const NAME = '@kin-kinetic/sdk'
-export const VERSION = '1.0.0-rc.6'
+export const VERSION = '1.0.0-rc.7'
