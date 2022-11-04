af7da129547e25c513baec5bb8070270cfa7f7d4
Simon Howard <showered@me.com>
2022-11-04 22:02:18 +0000

    fix: Default skipPreflight to false

--- libs/api/transaction/data-access/src/lib/api-transaction-data-access.service.ts
+++ libs/api/transaction/data-access/src/lib/api-transaction-data-access.service.ts
@@ -445,7 +445,7 @@ export class ApiTransactionDataAccessService implements OnModuleInit {
   ): Promise<TransactionWithErrors> {
     const solanaStart = new Date()
     try {
-      const signature = await solana.sendRawTransaction(solanaTransaction, { skipPreflight: true })
+      const signature = await solana.sendRawTransaction(solanaTransaction)
       const status = TransactionStatus.Committed
       const solanaCommitted = new Date()
       const solanaCommittedDuration = solanaCommitted.getTime() - solanaStart.getTime()
