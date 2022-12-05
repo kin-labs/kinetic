2834183e0566867db4f20a886770e0810ab46f77
Bram Borggreve <beeman@beeman.nl>
2022-12-05 20:06:05 +0000

    fix: make sure api-queue-data-access-service test has correct imports

--- libs/api/queue/data-access/src/lib/api-queue-data-access.service.spec.ts
+++ libs/api/queue/data-access/src/lib/api-queue-data-access.service.spec.ts
@@ -1,11 +1,14 @@
+import { ApiCoreDataAccessModule } from '@kin-kinetic/api/core/data-access'
 import { Test } from '@nestjs/testing'
 import { ApiQueueDataAccessService } from './api-queue-data-access.service'
+import { ApiQueueCloseAccountModule } from './queue/close-account/api-queue-close-account.module'
 
 describe('ApiQueueDataAccessService', () => {
   let service: ApiQueueDataAccessService
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
+      imports: [ApiCoreDataAccessModule, ApiQueueCloseAccountModule],
       providers: [ApiQueueDataAccessService],
     }).compile()
 
