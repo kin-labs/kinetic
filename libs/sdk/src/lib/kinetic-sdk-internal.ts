dff7463bd6c304bf394a363d66e25bf666a643e8
beeman <beeman@beeman.nl>
2022-12-26 23:18:41 +0000

    refactor: use getAccountInfo to fetch token accounts on makeTransferBatch (#549)
    * refactor: use getAccountInfo to fetch owner and destination token accounts on makeTransfer
    
    * refactor: use getAccountInfo to fetch token accounts on makeTransferBatch

--- libs/sdk/src/lib/kinetic-sdk-internal.ts
+++ libs/sdk/src/lib/kinetic-sdk-internal.ts
@@ -300,20 +300,65 @@ export class KineticSdkInternal {
     const referenceId = options.referenceId || null
     const referenceType = options.referenceType || null
 
-    this.validateDestinations(appConfig, destinations)
+    if (destinations?.length < 1) {
+      throw new Error('At least 1 destination required')
+    }
+
+    if (destinations?.length > 15) {
+      throw new Error('Maximum number of destinations exceeded')
+    }
+
+    // We get the token account for the owner
+    const ownerTokenAccount = await this.findTokenAccount({
+      account: options.owner.publicKey,
+      commitment,
+      mint: mint.publicKey,
+    })
+
+    // The operation fails if the owner doesn't have a token account for this mint
+    if (!ownerTokenAccount) {
+      throw new Error(`Owner account doesn't exist for mint ${mint.publicKey}.`)
+    }
+
+    // Get TokenAccount from destinations, keep track of missing ones
+    const nonExistingDestinations: string[] = []
+    const destinationInfo: { amount: string; destination?: string }[] = await Promise.all(
+      destinations.map(async (d) => {
+        const destination = await this.findTokenAccount({
+          account: d.destination.toString(),
+          commitment,
+          mint: mint.publicKey,
+        })
+        if (!destination) {
+          nonExistingDestinations.push(d.destination.toString())
+        }
+        return {
+          amount: d.amount,
+          destination: destination?.toString(),
+        }
+      }),
+    )
+
+    // The operation fails if any of the destinations doesn't have a token account for this mint
+    if (nonExistingDestinations.length) {
+      throw new Error(
+        `Destination accounts ${nonExistingDestinations.join(', ')} have no token account for mint ${mint.publicKey}.`,
+      )
+    }
 
     const { blockhash, lastValidBlockHeight } = await this.getBlockhash()
 
     const tx = await generateMakeTransferBatchTransaction({
       addMemo: mint.addMemo,
       blockhash,
-      destinations,
+      destinations: destinationInfo as TransferDestination[],
       index: this.sdkConfig.index,
       lastValidBlockHeight,
       mintDecimals: mint.decimals,
       mintFeePayer: mint.feePayer,
       mintPublicKey: mint.publicKey,
       owner: options.owner.solana,
+      ownerTokenAccount,
       type: options.type || TransactionType.None,
     })
 
@@ -417,21 +462,5 @@ export class KineticSdkInternal {
 
   private makeTransferRequest(request: MakeTransferRequest) {
     return this.transactionApi.makeTransfer(request).then((res) => res.data)
-  }
-
-  private validateDestination(appConfig: AppConfig, destination: string) {
-    if (appConfig.mints.find((mint) => mint.publicKey === destination)) {
-      throw new Error(`Transfers to a mint are not allowed.`)
-    }
-  }
-
-  private validateDestinations(appConfig: AppConfig, destinations: TransferDestination[]) {
-    if (destinations?.length < 1) {
-      throw new Error('At least 1 destination required')
-    }
-    if (destinations?.length > 15) {
-      throw new Error('Maximum number of destinations exceeded')
-    }
-    destinations.forEach((transfer) => this.validateDestination(appConfig, transfer.destination?.toString()))
   }
 }
