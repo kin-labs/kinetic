e09785bccc67c4b413fce9ee046a6100174591b4
beeman ⬢ <beeman@beeman.nl>
2023-01-26 23:39:02 +0000

    feat: support environments with a dash in their name (#566)

--- libs/api/core/util/src/lib/get-parse-app-key.ts
+++ libs/api/core/util/src/lib/get-parse-app-key.ts
@@ -8,7 +8,12 @@ export function getAppKey(environment: string, index: number): string {
 }
 
 export function parseAppKey(appKey: string): { environment: string; index: number } {
-  const [index, environment] = appKey.replace(`${APP_KEY_PREFIX}-`, '').split('-')
+  // Remove the prefix from the appKey
+  const clean = appKey.replace(`${APP_KEY_PREFIX}-`, '')
+  // The first part of the appKey is the index of the app
+  const [index] = clean.split('-')
+  // The rest of the appKey is the environment
+  const environment = clean.replace(`${index}-`, '')
 
   return { environment, index: Number(index) }
 }
